68f052d3f6de68ee8111347a07390e820ab42c2b
Mattias Jonsson <mattias.jonsson@sogeti.se>
2022-12-02 09:40:42 +0100

    WIP Day2

---
+++ src/day2/day2.ts
@@ -0,0 +1,21 @@
+
+type Rock = 'A'  | 'X';
+type Paper = 'B' | 'Y';
+type Scissors = 'C' | 'Z';
+
+type Hand = Rock|Paper|Scissors;
+
+const matchPoints:Record<string, number> = {
+    'win': 6,
+    'loss': 0,
+    'draw': 3
+}
+
+const handPoints:Record<Hand, number> = {
+    'A'| 'X'): 
+}
+export function calculateWinner(a:Hand,b:Hand):string {
+    
+ }
+
+ export function 
