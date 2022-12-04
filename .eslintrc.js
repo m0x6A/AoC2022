f222b099cf83912d72469cb84464b5e1a3580ea2
Mattias Jonsson <xdr@xdr.nu>
2022-12-04 10:02:08 +0100

    Remove react specific typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,9 +16,6 @@ module.exports = {
   },
 
   plugins: [
-    'react',
-    'react-hooks',
-    'jsx-a11y',
     '@typescript-eslint',
     'prettier',
   ],
@@ -34,11 +31,7 @@ module.exports = {
   },
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
     'eslint:recommended',
-    'airbnb',
-    'airbnb/hooks',
-    'airbnb-typescript',
     'prettier',
   ],
   // overrides: [
@@ -48,38 +41,10 @@ module.exports = {
   // ],
   rules: {
     'no-use-before-define': 'off',
-    'react/react-in-jsx-scope': 'off',
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': 'error',
-    'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
 
     'no-shadow': 'off',
     '@typescript-eslint/no-shadow': ['error'],
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
-    'no-param-reassign': ['error', { props: false }],
-    'react/prop-types': 'off', // Since we do not use prop-types
-    'react/require-default-props': 'off', // Since we do not use prop-types
-    'react/function-component-definition': [
-      2,
-      {
-        namedComponents: 'arrow-function',
-        unnamedComponents: 'arrow-function',
-      },
-    ],
-    'react/jsx-props-no-spreading': [
-      2,
-      {
-        custom: 'ignore',
-      },
-    ],
-  },
+  }
 };
