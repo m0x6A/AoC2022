0828992650a6928a9e118c26bdde25f475ee89c9
Mattias, Jonsson <xdr@xdr.nu>
2022-12-06 22:14:01 +0100

    Cleanup
    * Solve day 6
    
    * Rename start command
    
    * Clean up

--- src/day6/day6.ts
+++ src/day6/day6.ts
@@ -1,8 +1,9 @@
 
 export function findPacket(stream: string, distinct:number): number {
-  const chars = stream.split('');
-  return chars.map((_, index) => {
-    const packet = chars.slice(index, index + distinct);    
-    return (index > 0 && packet.filter((p,i) => i === packet.indexOf(p))?.length === distinct) ? index + distinct : 0;
+  return stream.split('').map((_, index) => {
+    const packet = [...stream.slice(index, index + distinct)];    
+    return (index > 0 && packet.filter((p,i) => i === packet.indexOf(p))?.length === distinct) 
+      ? index + distinct : 0;
   }).find((x) => x > 0) ?? 0;
 }
+
