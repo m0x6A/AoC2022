48dd812931233c8ebd8f0b0739952a42c1b499f4
Mattias, Jonsson <xdr@xdr.nu>
2022-12-07 10:16:46 +0100

    Add alternate solution (#13)

--- src/day6/day6.ts
+++ src/day6/day6.ts
@@ -1,9 +1,7 @@
 
 export function findPacket(stream: string, distinct:number): number {
   return stream.split('').map((_, index) => {
-    const packet = [...stream.slice(index, index + distinct)];    
-    return (index > 0 && packet.filter((p,i) => i === packet.indexOf(p))?.length === distinct) 
+    return (index > 0 && new Set([...stream.slice(index, index + distinct)]).size === distinct) 
       ? index + distinct : 0;
   }).find((x) => x > 0) ?? 0;
 }
-
